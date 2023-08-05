9ab201cc52bd6b35e361f1af036a013fe1800947
schadenfreude <zeldon.zoom@gmail.com>
2023-08-05 16:43:12 -0500

    initial commit

---
+++ flaskblog/forms.py
@@ -0,0 +1,72 @@
+from flask_wtf import FlaskForm
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length,  Email, EqualTo, ValidationError
+from email_validator import validate_email
+from flaskblog.models import User
+
+
+
+
+class RegistraionForm(FlaskForm):
+
+    username=StringField('Username', 
+                        validators=[DataRequired(), Length(min=5, max=22) ])
+
+    email=StringField('Email',
+                        validators=[DataRequired(), Email()])
+
+    password=PasswordField('Password', 
+                        validators=[DataRequired(),Length(min=5, max=20)] )
+
+    confirm_password=PasswordField('Confirm Password', 
+                        validators=[DataRequired(),Length(min=5, max=20), EqualTo('password')])
+
+    submit=SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already taken. Please choose a different one')
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email already taken. Please choose a different one')
+
+
+class LoginForm(FlaskForm):
+
+    email=StringField('Email',
+                        validators=[DataRequired(), Email()])
+
+    password=PasswordField('Password', 
+                        validators=[DataRequired(),Length(min=5, max=20)])
+
+    remember= BooleanField('Remember Me ')
+
+    submit=SubmitField('Log In')
+
+
+class UpdateAccountForm(FlaskForm):
+
+    username=StringField('Username', 
+                        validators=[DataRequired(), Length(min=5, max=22) ])
+
+    email=StringField('Email',
+                        validators=[DataRequired(), Email()])
+    
+
+    submit=SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username already taken. Please choose a different one')
+        
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already taken. Please choose a different one')
---
+++ flaskblog/routes.py
@@ -0,0 +1,87 @@
+from flask import render_template, url_for, flash, redirect, request
+from flaskblog.forms import RegistraionForm, LoginForm, UpdateAccountForm
+from flaskblog import app, bcrypt, db
+from flaskblog.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+
+
+
+posts=[
+    {
+    "author":"Kanye Waste",
+    "title":"Post title 1",
+    "content":"Such a beautiful day today!",
+    "date_posted":"20 April, 2021",
+    },
+    
+    {
+    "author":"Post Melon",
+    "title":"Post title 2",
+    "content":"If I were you??",
+    "date_posted":"20 April, 2023",
+    }
+]
+
+
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts, title='Home')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+         return redirect(url_for('home'))
+    form = RegistraionForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password= hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created. You are now able to login', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+         return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user, remember= form.remember.data)
+                next_page = request.args.get('next')
+                return redirect(next_page)  if next_page else redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check your email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account", methods=['POST', 'GET'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+         current_user.username = form.username.data
+         current_user.email = form.email.data
+         db.session.commit()
+         flash('Your Account has been updated', 'success')
+         return redirect(url_for('account'))
+    image_file = url_for('static', filename='Profile_Pictures/'+f'{current_user.image_file}')
+    return render_template('account.html', title='Account', image_file = image_file, form=form)
+
+    
+
