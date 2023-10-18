9748c76b33888202c6e34cc2c5df7f55b265787d
schadenfreude <zeldon.zoom@gmail.com>
2023-10-17 23:04:45 -0500

    added update & delete post button

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -129,6 +129,21 @@ def update_post(post_id):
     post = Post.query.get_or_404(post_id)
     if post.author != current_user:
          abort(403)
-    form.title.data=post.title
-    form.content.data=post.content
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Your post has been updated!", "success")
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data=post.title
+        form.content.data=post.content
     return render_template('create_post.html', title='Update Post', post=post, form=form, legend='Update Post')
+
+app.route("/post/<int:post_id>/update", methods=['POST', 'GET'])
+@login_required
+def update_post(post_id):
+    form=PostForm()
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+         abort(403)
